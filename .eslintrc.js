c935a783242debcb429d0a01b88cb9520eec3bc8
ermekm <ermek.mrzv@gmail.com>
2023-05-02 18:18:42 +0600

    fixed

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,64 @@
 module.exports = {
     env: {
         browser: true,
-        es2021: true
+        es2021: true,
+        jest: true,
     },
     extends: [
-        'plugin:react/jsx-runtime',
-        'standard-with-typescript',
-        'plugin:i18next/recommended'
+        'plugin:react/recommended',
+        'airbnb',
+        'plugin:i18next/recommended',
     ],
-    overrides: [
-    ],
+    parser: '@typescript-eslint/parser',
     parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
         ecmaVersion: 'latest',
         sourceType: 'module',
-        project: ['./tsconfig.json']
     },
     plugins: [
         'react',
-        'i18next'
+        '@typescript-eslint',
+        'i18next',
     ],
     rules: {
-        indent: 'off',
-        '@typescript-eslint/indent': [2, 4],
-        'i18next/no-literal-string': ['error', { markupOnly: true }],
-        '@typescript-eslint/prefer-nullish-coalescing': 'off',
-        '@typescript-eslint/strict-boolean-expressions': 'off'
+        'react/jsx-indent': [2, 4],
+        'react/jsx-indent-props': [2, 4],
+        indent: [2, 4],
+        'react/jsx-filename-extension': [
+            2,
+            { extensions: ['.js', '.jsx', '.tsx'] },
+        ],
+        'import/no-unresolved': 'off',
+        'import/prefer-default-export': 'off',
+        'no-unused-vars': 'warn',
+        'react/require-default-props': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-props-no-spreading': 'warn',
+        'react/function-component-definition': 'off',
+        'no-shadow': 'off',
+        'import/extensions': 'off',
+        'import/no-extraneous-dependencies': 'off',
+        'no-underscore-dangle': 'off',
+        'i18next/no-literal-string': [
+            'error',
+            {
+                markupOnly: true,
+                ignoreAttribute: ['data-testid', 'to'],
+            },
+        ],
+        'max-len': ['error', { ignoreComments: true, code: 100 }],
     },
     globals: {
-        IS_DEV: true
-    }
-}
+        __IS_DEV__: true,
+    },
+    overrides: [
+        {
+            files: ['**/src/**/*.test.{ts,tsx}'],
+            rules: {
+                'i18next/no-literal-string': 'off',
+            },
+        },
+    ],
+};
